c118d0f2e5c86416ae5f3c52c31d9877e9986b84
Evgeny Saltykov <es@zanami.ru>
2015-10-27 16:46:20 +1000

    product tpl nopanels

--- template.php
+++ template.php
@@ -13,7 +13,7 @@
 function atk_process_page(&$variables) {
 
 	 // If panels arent being used at all. From kalatheme
-   $variables['no_panels'] = !(module_exists('page_manager') && page_manager_get_current_page());
-	 $variables['no_panels'] = !($variables['no_panels'] && isset($variables['node']->type) && $variables['node']->type == 'product');
+   $variables['no_panels'] = !;
+	 $variables['no_panels'] = !((isset($variables['node']->type) && $variables['node']->type == 'product') || (module_exists('page_manager') && page_manager_get_current_page()));
 
 }
