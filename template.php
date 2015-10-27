fc48661e8a82b9d8074160999a82f5f87cf16674
Evgeny Saltykov <es@zanami.ru>
2015-10-27 16:35:23 +1000

    product tpl

--- template.php
+++ template.php
@@ -3,3 +3,15 @@
  * @file 
  * Primary pre/preprocess functions and alterations.
  */ 
+
+
+/**
+ * Override or insert variables into the page template.
+ *
+ * Implements template_process_page().
+ */
+function atk_process_page(&$variables) {
+
+	 $variables['no_panels'] = !(isset($variables['node']->type) && $variables['node']->type == 'product');
+
+}
