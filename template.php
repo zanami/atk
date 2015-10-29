b8e7cdfa5f47fd8a2b0eb62e9c8091223dcfb552
Evgeny Saltykov <es@zanami.ru>
2015-10-29 14:12:12 +1000

    kalatheme menu depth navbar

--- template.php
+++ template.php
@@ -14,5 +14,10 @@ function atk_process_page(&$variables) {
 
 	 // If panels arent being used at all. From kalatheme
 	 $variables['no_panels'] = !((isset($variables['node']->type) && $variables['node']->type == 'product') || (module_exists('page_manager') && page_manager_get_current_page()));
-
+	 // menu depth override kalatheme
+	 // Get the entire main menu tree.
+	  $main_menu_tree = array();
+	  $main_menu_tree = menu_tree_all_data('main-menu', NULL, 1);
+	  // Add the rendered output to the $main_menu_expanded variable.
+	  $variables['main_menu_expanded'] = menu_tree_output($main_menu_tree);
 }
